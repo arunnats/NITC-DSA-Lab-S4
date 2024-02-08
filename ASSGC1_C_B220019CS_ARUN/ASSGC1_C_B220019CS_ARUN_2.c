1c2c58b22a048cf7d284778f838db0c6e03ae531
arunnats <arunnats2004@gmail.com>
2024-02-08 17:09:19 +0530

    Q2 kind of works, needs work

--- ASSGC1_C_B220019CS_ARUN/ASSGC1_C_B220019CS_ARUN_2.c
+++ ASSGC1_C_B220019CS_ARUN/ASSGC1_C_B220019CS_ARUN_2.c
@@ -7,7 +7,6 @@
 struct Node
 {
     int key;
-    int value;
     struct Node *next;
     struct Node *prev;
 };
@@ -17,38 +16,170 @@ struct HashMap
     struct Node *array[SIZE];
 };
 
-struct HashMap *createHashMap()
+struct HashMap *createHashMap(int maxSize)
 {
     struct HashMap *map = (struct HashMap *)malloc(sizeof(struct HashMap));
-    for (int i = 0; i < SIZE; i++)
+    for (int i = 0; i < maxSize; i++)
     {
         map->array[i] = NULL;
     }
     return map;
 }
 
-int hashFunction(int key, int tableSize)
+int hashFunction(int key, int maxSize)
 {
-    return key % tableSize;
+    return key % maxSize;
 }
 
-void insert(struct HashMap *map, int key, int value)
+void insert(struct HashMap *map, int key, int maxSize)
 {
-    int index = hashFunction(key);
+    int index = hashFunction(key, maxSize);
+
     struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
-    strcpy(newNode->key, key);
-    newNode->value = value;
-    newNode->next = map->array[index];
-    map->array[index] = newNode;
+    newNode->key = key;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+
+    if (map->array[index] == NULL)
+    {
+        map->array[index] = newNode;
+    }
+    else
+    {
+        struct Node *current = map->array[index];
+        while (current->next != NULL)
+        {
+            current = current->next;
+        }
+        current->next = newNode;
+        newNode->prev = current;
+    }
 }
 
-int search(struct HashMap *map, int key)
+void search(struct HashMap *map, int key, int maxSize)
 {
-    int index = hashFunction(key);
+    int index = hashFunction(key, maxSize);
     struct Node *current = map->array[index];
+
+    int position = 1;
+
+    while (current != NULL)
+    {
+        if (current->key == key)
+        {
+            printf("%d %d\n", index, position);
+            return;
+        }
+        current = current->next;
+        position++;
+    }
+
+    printf("-1\n");
+}
+
+void delete(struct HashMap *map, int key, int maxSize)
+{
+    int index = hashFunction(key, maxSize);
+    struct Node *current = map->array[index];
+
+    int position = 1;
+
+    while (current != NULL)
+    {
+        if (current->key == key)
+        {
+            if (current->prev != NULL)
+            {
+                current->prev->next = current->next;
+            }
+            else
+            {
+                map->array[index] = current->next;
+            }
+
+            if (current->next != NULL)
+            {
+                current->next->prev = current->prev;
+            }
+
+            free(current);
+            printf("%d %d\n", index, position);
+            return;
+        }
+        current = current->next;
+        position++;
+    }
+
+    printf("-1\n");
+}
+
+void update(struct HashMap *map, int oldKey, int newKey, int maxSize)
+{
+    delete (map, oldKey, maxSize);
+    insert(map, newKey, maxSize);
+}
+
+void printElementsInChain(struct HashMap *map, int index, int maxSize)
+{
+    if (index < 0 || map->array[index] == NULL)
+    {
+        printf("-1\n");
+        return;
+    }
+
+    struct Node *current = map->array[index];
+    while (current != NULL)
+    {
+        printf("%d ", current->key);
+        current = current->next;
+    }
+    printf("\n");
 }
 
 int main()
 {
-    struct HashMap *map = createHashMap();
+    int maxSize;
+    scanf("%d", &maxSize);
+    struct HashMap *map = createHashMap(maxSize);
+    int key, oldKey, newKey, index;
+    char option;
+    do
+    {
+        scanf(" %c", &option);
+        switch (option)
+        {
+        case 'i':
+            scanf("%d", &key);
+            insert(map, key, maxSize);
+            break;
+
+        case 'd':
+            scanf("%d", &key);
+            delete (map, key, maxSize);
+            break;
+
+        case 'u':
+            scanf("%d %d", &oldKey, &newKey);
+            update(map, oldKey, newKey, maxSize);
+            break;
+
+        case 's':
+            scanf("%d", &key);
+            search(map, key, maxSize);
+            break;
+
+        case 'p':
+            scanf("%d", &index);
+            printElementsInChain(map, index, maxSize);
+            break;
+
+        case 'e':
+            break;
+        }
+
+    } while (option != 'e');
+
+    free(map);
+
+    return 1;
 }

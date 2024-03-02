919fc78b393dd3a27c9c9637bb4905299574cedc
arunnats <arunnats2004@gmail.com>
2024-03-02 21:53:46 +0530

    implemented avl insert

--- avlTree/avlTree1_insert.c
+++ avlTree/avlTree1_insert.c
@@ -153,6 +153,90 @@ void printLevelOrder(struct Node *root)
     }
 }
 
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+
+int height(struct Node *node)
+{
+    if (node == NULL)
+        return -1;
+    return node->height;
+}
+
+int getBalance(struct Node *node)
+{
+    if (node == NULL)
+        return 0;
+    return height(node->l) - height(node->r);
+}
+
+struct Node *rightRotate(struct Node *y)
+{
+    struct Node *x = y->l;
+    struct Node *T2 = x->r;
+
+    x->r = y;
+    y->l = T2;
+
+    y->height = max(height(y->l), height(y->r)) + 1;
+    x->height = max(height(x->l), height(x->r)) + 1;
+
+    return x;
+}
+
+struct Node *leftRotate(struct Node *x)
+{
+    struct Node *y = x->r;
+    struct Node *T2 = y->l;
+
+    y->l = x;
+    x->r = T2;
+
+    x->height = max(height(x->l), height(x->r)) + 1;
+    y->height = max(height(y->l), height(y->r)) + 1;
+
+    return y;
+}
+
+struct Node *insertAVL(struct Node *root, int key)
+{
+    if (root == NULL)
+        return initializeNode(key);
+
+    if (key < root->key)
+        root->l = insertAVL(root->l, key);
+    else if (key > root->key)
+        root->r = insertAVL(root->r, key);
+    else
+        return root;
+
+    root->height = 1 + max(height(root->l), height(root->r));
+
+    int balance = getBalance(root);
+
+    if (balance > 1 && key < root->l->key)
+        return rightRotate(root);
+
+    if (balance < -1 && key > root->r->key)
+        return leftRotate(root);
+
+    if (balance > 1 && key > root->l->key)
+    {
+        root->l = leftRotate(root->l);
+        return rightRotate(root);
+    }
+
+    if (balance < -1 && key < root->r->key)
+    {
+        root->r = rightRotate(root->r);
+        return leftRotate(root);
+    }
+
+    return root;
+}
+
 int main()
 {
     struct Node *root = NULL;
@@ -168,7 +252,7 @@ int main()
         {
         case 'a':
             scanf(" %d", &x);
-            root = insertNode(root, x);
+            root = insertAVL(root, x);
             break;
 
         case 'r':

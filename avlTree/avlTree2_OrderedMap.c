eba01973c926450e03fcacd4abaa22ce5f767fd7
arunnats <arunnats2004@gmail.com>
2024-03-03 13:37:26 +0530

    started fixed lower bound code

--- avlTree/avlTree2_OrderedMap.c
+++ avlTree/avlTree2_OrderedMap.c
@@ -108,7 +108,22 @@ struct Node *insert(struct Node *node, int key, int value)
     return node;
 }
 
-struct AVLNode *lowerBound(struct AVLNode *node, int key)
+struct Node *lowerBound(struct Node *node, int key)
+{
+    if (node == NULL)
+        return NULL;
+
+    if (key <= node->key)
+    {
+        struct Node *leftLB = lowerBound(node->l, key);
+
+        return (leftLB != NULL) ? leftLB : node;
+    }
+
+    return lowerBound(node->r, key);
+}
+
+struct Node *find(struct Node *node, int key)
 {
     if (node == NULL)
         return NULL;
@@ -117,9 +132,91 @@ struct AVLNode *lowerBound(struct AVLNode *node, int key)
         return node;
 
     if (key < node->key)
-        return lowerBound(node->left, key);
+        return find(node->l, key);
+    else
+        return find(node->r, key);
+}
 
-    struct AVLNode *rightLB = lowerBound(node->right, key);
+int size(struct Node *node)
+{
+    if (node == NULL)
+        return 0;
 
-    return (rightLB != NULL) ? rightLB : node;
+    return 1 + size(node->l) + size(node->r);
+}
+
+int empty(struct Node *root)
+{
+    return (root == NULL) ? 1 : 0;
+}
+
+void printElements(struct Node *node)
+{
+    if (node == NULL)
+        return;
+
+    printElements(node->l);
+    printf("%d ", node->key);
+    printElements(node->r);
+}
+
+int main()
+{
+    struct Node *root = NULL;
+
+    char choice;
+    int key, value;
+
+    do
+    {
+        scanf(" %c", &choice);
+
+        switch (choice)
+        {
+        case 'i':
+            scanf("%d %d", &key, &value);
+            root = insert(root, key, value);
+            break;
+
+        case 'l':
+            scanf("%d", &key);
+            struct Node *lbResult = lowerBound(root, key);
+            if (lbResult != NULL)
+                printf("%d %d\n", lbResult->key, lbResult->value);
+            else
+                printf("-1\n");
+            break;
+
+        case 'f':
+            scanf("%d", &key);
+            struct Node *findResult = find(root, key);
+            if (findResult != NULL)
+                printf("%d %d\n", findResult->key, findResult->value);
+            else
+                printf("-1\n");
+            break;
+
+        case 's':
+            printf("%d\n", size(root));
+            break;
+
+        case 'e':
+            printf("%d\n", empty(root));
+            break;
+
+        case 'p':
+            printElements(root);
+            printf("\n");
+            break;
+
+        case 't':
+            break;
+
+        default:
+            printf("Invalid choice\n");
+        }
+
+    } while (choice != 't');
+
+    return 0;
 }

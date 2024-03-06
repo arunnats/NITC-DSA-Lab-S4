16c8aa690768a97271781b579edc1fc127f48fcc
arunnats <arunnats2004@gmail.com>
2024-03-06 17:51:26 +0530

    finished Q4

--- Cycle2/ASSGC2_A_B220019CS_ARUN/ASSGC2_A_B220019CS_ARUN_4.c
+++ Cycle2/ASSGC2_A_B220019CS_ARUN/ASSGC2_A_B220019CS_ARUN_4.c
@@ -1,76 +1,68 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#define RED 0
-#define BLACK 1
+#include <stdio.h>
 
 struct Node
 {
     int key;
-    int color;
-    struct Node *left;
-    struct Node *right;
-    struct Node *parent;
+    int colour;
+    struct Node *right, *left;
 };
 
-struct Node *initializeNode(int key, int color, struct Node *parent)
+struct Node *createNode(int key, int colour)
 {
     struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
     newNode->key = key;
-    newNode->color = color;
-    newNode->left = newNode->right = NULL;
-    newNode->parent = parent;
+    newNode->colour = colour;
+    newNode->left = NULL;
+    newNode->right = NULL;
     return newNode;
 }
 
-struct Node *buildTree(char ch[], int *i)
+struct Node *buildTree(char expression[], int *i)
 {
     int key = 0;
-    int c = 0;
+    int colour = 0;
     struct Node *root = NULL;
-    while (ch[*i] != '\0')
+    while (expression[*i] != '\0')
     {
-        if (ch[*i] == '(')
+        if (expression[*i] == '(')
         {
             (*i) += 2;
-            if (ch[*i] == ')')
+            if (expression[*i] == ')')
             {
                 (*i)++;
                 return NULL;
             }
             int s = 1;
-            if (ch[*i] == '-')
+            if (expression[*i] == '-')
             {
                 s = -1;
                 (*i)++;
             }
-            while (ch[*i] >= '0' && ch[*i] <= '9')
+            while (expression[*i] >= '0' && expression[*i] <= '9')
             {
-                key = key * 10 + (int)(ch[*i] - '0');
+                key = key * 10 + (int)(expression[*i] - '0');
                 (*i)++;
             }
             key *= s;
             (*i)++;
-            if (ch[*i] == 'R' || ch[*i] == 'r')
+            if (expression[*i] == 'R' || expression[*i] == 'r')
             {
-                c = RED;
+                colour = 0;
                 (*i) += 2;
             }
-            if (ch[*i] == 'B' || ch[*i] == 'b')
+            if (expression[*i] == 'B' || expression[*i] == 'b')
             {
-                c = BLACK;
+                colour = 1;
                 (*i) += 2;
             }
-            root = initializeNode(key, c, NULL);
-            (*i)++;
-            root->left = buildTree(ch, i);
-            (*i)++;
-            root->right = buildTree(ch, i);
-            (*i)++;
-            return root;
+            root = createNode(key, colour);
+            root->left = buildTree(expression, i);
+            root->right = buildTree(expression, i);
+            while (expression[*i] != ')')
+                (*i)++;
         }
-        else if (ch[*i] == ')')
+        else if (expression[*i] == ')')
         {
             (*i)++;
             return root;
@@ -81,55 +73,41 @@ struct Node *buildTree(char ch[], int *i)
     return root;
 }
 
-int checkRedBlackTree(struct Node *root, int count, int *valid)
+int checkRBCondition(struct Node *root, int count, int *valid)
 {
     if (root == NULL)
         return count;
-
-    if (root->color == 1)
+    if (root->colour == 1)
         count++;
-
-    if (root->color == 0 && ((root->left && root->left->color == 0) || (root->right && root->right->color == 0)))
+    if (root->colour == 0 && ((root->left && root->left->colour == 0) || (root->right && root->right->colour == 0)))
         *valid = 0;
-
-    int lc = checkRedBlackTree(root->left, count, valid);
-    int rc = checkRedBlackTree(root->right, count, valid);
-
+    int lc = checkRBCondition(root->left, count, valid);
+    int rc = checkRBCondition(root->right, count, valid);
     if (lc != rc)
         *valid = 0;
-
     return lc;
 }
 
 int main()
 {
     char expression[1001];
-    fgets(expression, sizeof(expression), stdin);
-    size_t len = strlen(expression);
+    fgets(expression, 1001, stdin);
 
-    if (len > 0 && expression[len - 1] == '\n')
-    {
-        expression[len - 1] = '\0';
-        len--;
-    }
+    struct Node *root = NULL;
 
-    int pos = 1;
-    struct Node *root = buildTree(expression, &pos);
-
+    int colour = 0;
+    root = buildTree(expression, &colour);
     int valid = 1;
     int count = 0;
-
-    if (root && root->color == 0)
+    if (root->colour == 0)
         valid = 0;
     else
     {
-        checkRedBlackTree(root, count, &valid);
+        checkRBCondition(root, count, &valid);
     }
-
     if (valid)
         printf("1\n");
     else
         printf("0\n");
-
-    return 1;
+    return 0;
 }

a9c9a42842c66e07e4c06524b65d6a8be3298599
arunnats <arunnats2004@gmail.com>
2024-03-31 17:37:57 +0530

    got the menu, jsut need to figue out the starting index

---
+++ graphs/primsAlgoMenu.c
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Graph
+{
+    int numVertices;
+    int **adjMatrix;
+};
+
+struct Graph *createGraph(int numVertices)
+{
+    struct Graph *graph = (struct Graph *)malloc(sizeof(struct Graph));
+    graph->numVertices = numVertices;
+
+    graph->adjMatrix = (int **)malloc(numVertices * sizeof(int *));
+    for (int i = 0; i < numVertices; ++i)
+    {
+        graph->adjMatrix[i] = (int *)malloc(numVertices * sizeof(int));
+        for (int j = 0; j < numVertices; ++j)
+        {
+            graph->adjMatrix[i][j] = 0;
+        }
+    }
+
+    return graph;
+}
+
+void addEdge(struct Graph *graph, int src, int dest, int weight)
+{
+    graph->adjMatrix[src][dest] = weight;
+}
+
+void printGraph(struct Graph *graph)
+{
+    printf("Adjacency matrix \n");
+
+    for (int i = 0; i < graph->numVertices; ++i)
+    {
+
+        for (int j = 0; j < graph->numVertices; ++j)
+        {
+            if (graph->adjMatrix[i][j] != 0)
+            {
+                printf("%d ", graph->adjMatrix[i][j]);
+            }
+            else
+                printf("x ");
+        }
+
+        printf("\n");
+    }
+}
+
+int minKey(int key[], int mstSet[], int m)
+{
+    // Initialize min value
+    int min = 9999, min_index;
+
+    for (int v = 0; v < m; v++)
+        if (mstSet[v] == 0 && key[v] < min)
+            min = key[v], min_index = v;
+
+    return min_index;
+}
+
+// A utility function to print the
+// constructed MST stored in parent[]
+void printMSTWeight(int parent[], struct Graph *graph)
+{
+    int weight = 0;
+    for (int i = 1; i < graph->numVertices; i++)
+        weight = weight + graph->adjMatrix[i][parent[i]];
+    printf("%d\n", weight);
+}
+
+void printMSTEdges(int parent[], struct Graph *graph)
+{
+    for (int i = 1; i < graph->numVertices; i++)
+        printf("%d %d ", i, parent[i]);
+}
+
+// Function to construct and print MST for
+// a graph represented using adjacency
+// matrix representation
+void primMST(struct Graph *graph, int ch)
+{
+    // Array to store constructed MST
+    int m = graph->numVertices;
+    int parent[m];
+    int key[m];
+    int mstSet[m];
+
+    // Initialize all keys as INFINITE
+    for (int i = 0; i < m; i++)
+        key[i] = 999, mstSet[i] = 0;
+
+    key[0] = 0;
+    parent[0] = -1;
+
+    // The MST will have V vertices
+    for (int count = 0; count < m - 1; count++)
+    {
+        int u = minKey(key, mstSet, m); // Pass 'm' as the number of vertices
+
+        mstSet[u] = 1;
+
+        for (int v = 0; v < m; v++)
+        {
+            if (graph->adjMatrix[u][v] && mstSet[v] == 0 && graph->adjMatrix[u][v] < key[v])
+                parent[v] = u, key[v] = graph->adjMatrix[u][v];
+        }
+    }
+
+    if (ch == 0)
+        printMSTWeight(parent, graph);
+    else if (ch == 1)
+        printMSTEdges(parent, graph);
+}
+
+int main()
+{
+    int m;
+    scanf("%d", &m);
+    struct Graph *graph = createGraph(m);
+
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            int weight;
+            scanf("%d", &weight);
+            if (weight)
+                addEdge(graph, i, j, weight);
+        }
+    }
+
+    char choice;
+    while (1)
+    {
+        scanf(" %c", &choice);
+        if (choice == 's')
+            primMST(graph, 1);
+        else if (choice == 't')
+            primMST(graph, 0);
+        else if (choice == 'p')
+            printGraph(graph);
+        else if (choice == 'e')
+            break;
+    }
+
+    return 0;
+}

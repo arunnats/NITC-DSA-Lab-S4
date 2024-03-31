34def391e1095b60c6be26af3906a76116bc1aa7
arunnats <arunnats2004@gmail.com>
2024-04-01 01:10:43 +0530

    working on q2, input format is quite strange

---
+++ graphs/weightedListInput.c
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_NODES 1000
+
+struct Node
+{
+    int destination;
+    int weight;
+    struct Node *next;
+};
+
+struct List
+{
+    struct Node *head;
+};
+
+struct Graph
+{
+    int V;
+    struct List *array;
+};
+
+struct Node *createNode(int destination)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->destination = destination;
+    newNode->weight = -1;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Graph *createGraph(int V)
+{
+    struct Graph *graph = (struct Graph *)malloc(sizeof(struct Graph));
+    graph->V = V;
+    graph->array = (struct List *)malloc(V * sizeof(struct List));
+    for (int i = 0; i < V; ++i)
+        graph->array[i].head = NULL;
+    return graph;
+}
+
+void addEdge(struct Graph *graph, int source, int destination)
+{
+    struct Node *newNode = createNode(destination);
+    newNode->next = graph->array[source].head;
+    graph->array[source].head = newNode;
+}
+
+void addWeight(struct Graph *graph, int source, int weight, int count)
+{
+    struct Node *temp = graph->array[source].head;
+
+    // if (temp->weight == -1)
+    // {
+    //     printf("weight = %d\n", weight);
+    //     printf("val = %d\n", temp->destination);
+    //     temp->weight = weight;
+    //     temp = temp->next;
+    // }
+
+    for (int i = 0; i < count; i++)
+    {
+        temp = temp->next;
+    }
+    temp->weight = weight;
+}
+
+void printGraph(struct Graph *graph)
+{
+    printf("Adjacency List Representation of the Graph:\n");
+    for (int v = 0; v < graph->V; ++v)
+    {
+        printf("Node %d -> ", v);
+        struct Node *temp = graph->array[v].head;
+        while (temp)
+        {
+            printf("(%d, %d) ", temp->destination, temp->weight);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int m;
+    scanf("%d", &m);
+    struct Graph *graph = createGraph(m);
+
+    int label, adj;
+    for (int i = 0; i < m; ++i)
+    {
+        scanf("%d", &label); // Read the label of the node
+        while (scanf("%d", &adj) == 1)
+        {
+            addEdge(graph, label, adj);
+            char c = getchar();
+            if (c == '\n')
+                break; // End of line
+        }
+    }
+
+    printGraph(graph);
+
+    for (int i = 0; i < m; ++i)
+    {
+        scanf("%d", &label); // Read the label of the node
+        int weight;
+        while (scanf("%d", &weight) == 1)
+        {
+            int count = 0;
+            addWeight(graph, label, weight, count);
+            count++;
+
+            printf("weight: %d\n", weight);
+            char c = getchar();
+            if (c == '\n')
+                break; // End of line
+        }
+    }
+
+    printGraph(graph);
+
+    return 0;
+}

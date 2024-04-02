5c3011250fc26097889279b69d290f82e4d98f44
arunnats <arunnats2004@gmail.com>
2024-04-02 22:07:35 +0530

    finished floyd warshall

---
+++ graphs/adjacencyMatricInputWeight.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Graph
+{
+    int numVertices;
+    int **adjMatrix;
+};
+
+struct Graph *createGraph(int numVertices)
+{
+    struct Graph *graph = (struct Graph *)malloc(sizeof(struct Graph));
+    graph->numVertices = numVertices;
+
+    graph->adjMatrix = (int **)malloc(numVertices * sizeof(int *));
+    for (int i = 0; i < numVertices; ++i)
+    {
+        graph->adjMatrix[i] = (int *)malloc(numVertices * sizeof(int));
+        for (int j = 0; j < numVertices; ++j)
+        {
+            graph->adjMatrix[i][j] = 0;
+        }
+    }
+
+    return graph;
+}
+
+void addEdge(struct Graph *graph, int src, int dest, int weight)
+{
+    graph->adjMatrix[src][dest] = weight;
+}
+
+void printGraph(struct Graph *graph)
+{
+    printf("Adjacency matrix \n");
+
+    for (int i = 0; i < graph->numVertices; ++i)
+    {
+
+        for (int j = 0; j < graph->numVertices; ++j)
+        {
+
+            printf("%d ", graph->adjMatrix[i][j]);
+        }
+
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int m;
+    scanf("%d", &m);
+    struct Graph *graph = createGraph(m);
+
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < m; ++j)
+        {
+            int weight;
+            scanf("%d", &weight);
+            if (weight)
+                addEdge(graph, i, j, weight);
+        }
+    }
+
+    printGraph(graph);
+
+    return 0;
+}

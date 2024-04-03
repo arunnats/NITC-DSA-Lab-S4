b5197b979c4db662d57b3ad0258faf008cafbf57
arunnats <arunnats2004@gmail.com>
2024-04-03 11:47:07 +0530

    finosihed q3

---
+++ Cycle3/ASSGC3_B_B220019CS_CS03_ARUN/ASSGC3_B_B220019CS_CS03_ARUN_3.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_NODES 1000
+
+struct Node
+{
+    int destination;
+    int weight;
+    struct Node *next;
+};
+
+struct List
+{
+    struct Node *head;
+};
+
+struct Graph
+{
+    int V;
+    struct List *array;
+};
+
+struct Node *createNode(int destination)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->destination = destination;
+    newNode->weight = -1;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Graph *createGraph(int V)
+{
+    struct Graph *graph = (struct Graph *)malloc(sizeof(struct Graph));
+    graph->V = V;
+    graph->array = (struct List *)malloc((V + 1) * sizeof(struct List));
+    for (int i = 1; i <= V; ++i)
+        graph->array[i].head = NULL;
+    return graph;
+}
+
+void addEdge(struct Graph *graph, int source, int destination)
+{
+    struct Node *newNode = createNode(destination);
+    struct Node *temp = graph->array[source].head;
+    if (temp == NULL)
+    {
+        graph->array[source].head = newNode;
+    }
+    else
+    {
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+void addWeight(struct Graph *graph, int source, int weight, int count)
+{
+    struct Node *temp = graph->array[source].head;
+    for (int i = 1; i <= count; i++)
+    {
+        temp = temp->next;
+    }
+    temp->weight = weight;
+}
+
+void printGraph(struct Graph *graph)
+{
+    for (int v = 1; v <= graph->V; ++v)
+    {
+        printf("Node %d -> ", v);
+        struct Node *temp = graph->array[v].head;
+        while (temp)
+        {
+            printf("(%d, %d) ", temp->destination, temp->weight);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+
+void dijkstra(struct Graph *graph, int V, int src)
+{
+    int dist[V + 1];
+    int visited[V + 1];
+    for (int i = 1; i <= V; ++i)
+    {
+        dist[i] = 999;
+        visited[i] = 0;
+    }
+
+    dist[src] = 0;
+
+    for (int count = 0; count < V - 1; ++count)
+    {
+        int min_dist = 999;
+        int u;
+        for (int v = 1; v <= V; ++v)
+        {
+            if (visited[v] == 0 && dist[v] < min_dist)
+            {
+                min_dist = dist[v];
+                u = v;
+            }
+        }
+
+        visited[u] = 1;
+
+        struct Node *temp = graph->array[u].head;
+        while (temp)
+        {
+            int v = temp->destination;
+            if (!visited[v] && dist[u] != 999 && dist[u] + temp->weight < dist[v])
+            {
+                dist[v] = dist[u] + temp->weight;
+            }
+            temp = temp->next;
+        }
+    }
+
+    int i;
+    for (i = 1; i < V; i++)
+    {
+        printf("%d ", dist[i]);
+    }
+    printf("%d", dist[i]);
+}
+
+int main()
+{
+    int m;
+    scanf("%d", &m);
+    struct Graph *graph = createGraph(m);
+
+    int label, adj;
+    for (int i = 1; i <= m; ++i)
+    {
+        scanf("%d", &label);
+        while (scanf("%d", &adj) == 1)
+        {
+            addEdge(graph, label, adj);
+            char c = getchar();
+            if (c == '\n')
+                break;
+        }
+    }
+
+    for (int i = 1; i <= m; ++i)
+    {
+        scanf("%d", &label);
+        int weight;
+        int count = 0;
+        while (scanf("%d", &weight) == 1)
+        {
+            addWeight(graph, label, weight, count);
+            count++;
+            char c = getchar();
+            if (c == '\n')
+                break;
+        }
+    }
+
+    int t;
+    scanf("%d", &t);
+
+    if (t < 1 || t > m)
+    {
+        printf("-1\n");
+        return 0;
+    }
+
+    dijkstra(graph, m, t);
+
+    return 1;
+}
